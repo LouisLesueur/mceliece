34fa341a842540c93f6ccc00cc118bea20b8203b
Louis Lesueur <louis.lesueur@gmx.fr>
2020-04-21 23:49:23 +0200

    main

---
+++ main.py
@@ -0,0 +1,15 @@
+from mceliece.mceliececipher import McElieceCipher
+import numpy as np
+
+mceliece = McElieceCipher(4, 15, 2)
+mceliece.generate_random_keys()
+
+# Obligé car pas numpy par défaut
+mceliece.Gp = np.array(mceliece.Gp)
+mceliece.H = np.array(mceliece.H)
+mceliece.P = np.array(mceliece.P)
+mceliece.P_inv = np.array(mceliece.P_inv)
+
+cypher = mceliece.encrypt(np.array([0, 1, 0, 1, 0, 1, 0]))
+
+print(cypher)
