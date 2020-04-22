f57a3e2f1d997fa48c4282ac6e9fc4c849c19f56
Nicolas Birac <nicolas.birac@gmail.com>
2020-04-22 10:42:23 +0200

    working debug

--- main.py
+++ main.py
@@ -1,13 +1,29 @@
 from mceliece.mceliececipher import McElieceCipher
+import logging
 import numpy as np
+import sys
 
-mceliece = McElieceCipher(4, 15, 2)
+root = logging.getLogger()
+root.setLevel(logging.DEBUG)
+ch = logging.StreamHandler(sys.stdout)
+#ch.setLevel(logging.DEBUG)
+ch.setLevel(logging.INFO)
+#ch.setLevel(logging.WARN)
+root.addHandler(ch)
+
+
+mceliece = McElieceCipher(6, 62, 8)
 mceliece.generate_random_keys()
 mceliece.to_numpy()
 
-message = np.array([0, 1, 0, 1, 0, 1, 0])
-print("Message original", message)
-cypher = mceliece.encrypt(message)
+
+message = "CD"
+bin_message = ""
+for e in message:
+    bin_message += bin(ord(e))[2:]
+bin_message = np.array(list(bin_message), dtype="int")
+print("Message original", bin_message)
+cypher = mceliece.encrypt(bin_message)
 print("Message crypté : ", cypher)
 decipher = mceliece.decrypt(cypher)
 print("Message décrypté :", decipher)
